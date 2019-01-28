71b606e0bb3d4e89e64b72a6205b44d79bce0ae9
Matthew Philps <philps.matthew@gmail.com>
2019-01-28 10:32:15 +0900

    levelWidth done

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -12,7 +12,19 @@
 // Answer: [1, 3, 2]
 
 function levelWidth(root) {
-    
+    const results = [0]
+    const queue = [root,"!"]
+    while(queue.length>1){
+        const node = queue.shift()
+        if(node === "!"){
+            queue.push("!")
+            results.push(0)
+        } else {
+            queue.push(...node.children)
+            results[results.length-1]++
+        }
+    }
+    return results
 }
 
 module.exports = levelWidth;
