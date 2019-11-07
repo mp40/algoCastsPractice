30287e2fdad3a2ba634ff8ec8c8f83562f63bb73
Matthew Philps <philps.matthew@gmail.com>
2019-11-08 08:08:17 +0900

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,47 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//     const vowels = ['a','e','i','o','u'];
+//     return str.split('').filter((char)=>{
+//         return vowels.includes(char.toLowerCase());
+//     }).length
+// }
+
+// function vowels(str) {
+//     const vowels = ['a','e','i','o','u'];
+//     return str.split('').reduce((acc, char)=>{
+//         return vowels.includes(char.toLowerCase()) ? acc + 1 : acc + 0;
+//     },0)
+// }
+
+// function vowels(str) {
+//     let count = 0;
+//     const vowelString = 'aeiou';
+
+//     for (let char of str.toLowerCase()) {
+//         if(vowelString.includes(char)) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// function vowels(str) {
+//     let count = 0;
+//     const vowelArray = ['a','e','i','o','u'];
+
+//     for (let char of str.toLowerCase()) {
+//         if(vowelArray.includes(char)) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+function vowels(str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
